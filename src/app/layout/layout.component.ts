fae971423eb231998fa527ad311c963bd5899796
RSamaium <sam@newbot.io>
2023-02-06 10:39:44 +0100

    user service

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,8 +3,10 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-layout',
     template: `
-        <app-navbar></app-navbar>
-        <router-outlet></router-outlet>
+        <div class="container">
+            <app-navbar></app-navbar>
+            <router-outlet></router-outlet>
+        </div>
     `
 })
 export class LayoutComponent {}
