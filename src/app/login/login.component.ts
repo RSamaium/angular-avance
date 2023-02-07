4c31afb9aa0da2e9887bd22190f468fe5e4fbadc
RSamaium <sam@newbot.io>
2023-02-07 09:53:16 +0100

    check email

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,34 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Form, FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../core/services/auth.service';
 import { Router } from '@angular/router';
+import { UserService } from '../core/services/user.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  /*propEmail: FormControl = new FormControl('', [
+    Validators.required
+  ], [
+    this.userService.checkEmail.bind(this.userService)
+  ])*/
+  propEmail: FormControl = new FormControl('', {
+    validators: [Validators.required],
+    asyncValidators: [this.userService.checkEmail.bind(this.userService)],
+    updateOn: 'blur'
+  })
+  propPass: FormControl = new FormControl()
+  form: FormGroup = this.builder.group({
+    email: this.propEmail,
+    password: this.propPass
+  })
+
   //@ViewChild('form') myform!: NgForm
 
   /*ngAfterViewInit() {
@@ -20,11 +37,13 @@ export class LoginComponent {
 
   constructor(
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private builder: FormBuilder,
+    private userService: UserService
   ) { }
 
-  login(myform: NgForm) {
-    this.auth.login(myform.value).subscribe(() => {
+  login() {
+    this.auth.login(this.form.value).subscribe(() => {
       this.router.navigateByUrl('/')
     })
   }
